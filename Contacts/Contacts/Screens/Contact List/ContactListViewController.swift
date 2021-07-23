ec63a733b9c8d7c40fa1233033228decbfa51e5e
Miguel Askar <miguel.askar@globant.com>
2021-07-22 19:00:58 -0500

    Lista de contactos con apellido

---
+++ Contacts/Contacts/Screens/Contact List/ContactListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ConactListViewController.swift
+//  Contacts
+//
+//  Created by Miguel Angel Askar Rodriguez on 22/07/21.
+//
+
+import UIKit
+
+class ConactListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
